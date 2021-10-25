80f0cc4ead8640087d213266bfb0bbbb4eda0cd3
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-10-25 02:09:11 -0300

    02-The initial screen

--- src/components/initial.js
+++ src/components/initial.js
@@ -1,10 +1,54 @@
-
+import { useRef, useState } from 'react'
 
 const Initial = () => {
+    const textInput = useRef();
+    const [showNext,setShowNext] = useState(false);
+    const [showError,setShowError] = useState(false);
+
+    const handleChange = () => {
+        if(textInput.current.value.length >= 5) setShowNext(true)
+        else setShowNext(false);
+    }
+
+    const handleSubmit = () => {
+        const value = textInput.current.value;
+
+        if(value.length >= 30){
+            setShowError(true);
+            return false
+        }
+
+        alert('NEXT')
+    }
+ 
 
     return(
         <div>
-            initial
+            <h1>Ask a question</h1>
+            <input
+                ref={textInput}
+                onChange={handleChange}
+                name="question"
+                type="text"
+                className="form-control"
+            />
+
+            { showNext &&
+                <button
+                    className="btn"
+                    onClick={handleSubmit}
+                >
+                    Next
+                </button>
+            }
+
+            { showError &&
+                <div className="error">
+                    The question is too long
+                </div>       
+            }
+
+        
         </div>
     )
 
