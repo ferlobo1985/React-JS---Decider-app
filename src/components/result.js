f0bcf1065e2f90b712fc0e2b006046e58602fdce
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-10-25 03:01:32 -0300

    05-The results screen

--- src/components/result.js
+++ src/components/result.js
@@ -1,10 +1,30 @@
-
+import { useContext, useEffect } from 'react'
+import { MyContext } from '../context';
 
 const Result = () => {
+    const context = useContext(MyContext);
+
+    useEffect(()=>{
+        context.result()
+    },[])
 
     return(
         <div>
-            Result
+            <h3>Your answer is:</h3>
+            <div className="viewer">
+                {context.state.result}
+            </div>
+
+            <div className="animate__animated animate__bounceIn animate__delay-1s">
+                <hr/>
+                <button className="btn" onClick={context.reset}>
+                  Start over
+                </button>
+                <button className="btn" onClick={context.result}>
+                   Decide again
+                </button>
+            </div>
+
         </div>
     )
 
--- src/context/index.js
+++ src/context/index.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+const list  = [
+    'Yes','No','Maybe','Not sure..try again','Ask a friend','Call the police'
+]
 
 const MyContext = React.createContext();
 class MyProvider extends Component {
 
     state = {
         screen:0,
-        question:''
+        question:'',
+        result:''
     }
 
     handleGoTo = (value) => {
@@ -17,6 +22,29 @@ class MyProvider extends Component {
     }
 
 
+    getRandomValue = () => {
+        return list[Math.floor(Math.random()* list.length)]
+    }
+
+    handleResult = () => {
+        let rand = this.getRandomValue()
+
+        if(this.state.result !== ''){
+            while(rand === this.state.result){
+                rand = this.getRandomValue();
+            }
+        }
+        this.setState({result:rand})
+    }
+
+    handleReset = () => {
+        this.setState({
+            screen:0,
+            question:'',
+            result:''
+        })
+    }
+
 
     render(){
         return(
@@ -24,7 +52,9 @@ class MyProvider extends Component {
                 <MyContext.Provider value={{
                     state: this.state,
                     goTo:this.handleGoTo,
-                    question: this.handleQuestion
+                    question: this.handleQuestion,
+                    result: this.handleResult,
+                    reset: this.handleReset
                 }}
                 >
                     {this.props.children}
